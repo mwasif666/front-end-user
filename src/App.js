6f1f2cf415b748856bd42d873df9dc8ec8a52b97
WaleedGhori <waleedghori4@gmail.com>
2024-01-29 02:04:35 +0500

    whislist fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Nav_Bar from "./Components/navbar";
-
-
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { setFavProd } from "../../front-end-user/src/features/favCartSlice";
 const App = () => {
+ 
   return (
     <>
       <Nav_Bar />
-  
-
     </>
   );
 };
